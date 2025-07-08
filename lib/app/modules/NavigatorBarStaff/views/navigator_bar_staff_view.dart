c6e2d3f1831f15f4eb3c54557466cfb8557d0489
said el ouardy <openmobilecode2000@gmail.com>
2025-07-08 19:36:34 +0100

    side bar

--- lib/app/modules/NavigatorBarStaff/views/navigator_bar_staff_view.dart
+++ lib/app/modules/NavigatorBarStaff/views/navigator_bar_staff_view.dart
@@ -16,73 +16,112 @@ class NavigatorBarStaffView extends GetView<NavigatorBarStaffController> {
     return Scaffold(
       backgroundColor: ColorsApp.Grey6,
       body: SidebarX(
-            headerDivider: Container(
-              padding: EdgeInsets.symmetric(horizontal: 10 ,vertical: 10),
-              width: Get.width,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  CircleAvatar(
-                    foregroundImage: AssetImage(ImagesIconApp.logo),
-                    radius: 15,
-                  ),
-                  Text(
-                    'AAQAQIR_TIRGANIN'.tr,
-                    style: TextStyle(
-                      fontSize: Fonts.bodyLarge,
-                      fontWeight: FontWeight.w700,
-                      color: ColorsApp.Colors_gradientSplachscreen1,
-                    ),
-                  )
-                ],
+        controller: SidebarXController(selectedIndex: 0, extended: true),
+        headerDivider: Container(
+          padding: EdgeInsets.only(top: 20, bottom: 10),
+          width: Get.width,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              /////////////////////////////////////////////////////// header Divider
+              CircleAvatar(
+                foregroundImage: AssetImage(ImagesIconApp.logo),
+                radius: 15,
               ),
-            ),
-            controller: SidebarXController(selectedIndex: 0 , extended: true),
-            showToggleButton: false,
-            theme: SidebarXTheme(
-              width: 170,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withAlpha((0.1 * 255).toInt()),
-                    blurRadius: 12,
-                    offset: Offset(2, 4),
-                  ),
-                ],
-              ),
-              textStyle: TextStyle(
-                
-              )
-            ),
-            items: [
-              SidebarXItem(
-                iconBuilder:
-                    (context, selected) => SvgPicture.asset(
-                      ImagesIconApp.Home,
-                      width: 20,
-                      colorFilter: ColorFilter.mode(
-                        selected ? Colors.green : Colors.grey,
-                        BlendMode.srcIn,
-                      ),
-                    ),
-                label: 'Home',
-              ),
-          
-              SidebarXItem(
-                iconBuilder:
-                    (context, selected) => SvgPicture.asset(
-                      ImagesIconApp.Home,
-                      width: 20,
-                      colorFilter: ColorFilter.mode(
-                        selected ? Colors.green : ColorsApp.Colors_gradientSplachscreen2,
-                        BlendMode.srcIn,
-                      ),
-                    ),
-                label: 'Home',
+              SizedBox(width: 5),
+              Text(
+                'AAQAQIR_TIRGANIN'.tr,
+                style: TextStyle(
+                  fontSize: Fonts.bodyLarge,
+                  fontWeight: FontWeight.w700,
+                  color: ColorsApp.Colors_gradientSplachscreen1,
+                ),
               ),
             ],
           ),
+        ),
+        showToggleButton: false,
+        ///////////////////////////////////////////////////////////////////// controller
+        theme: SidebarXTheme(
+          width: 170,
+          selectedItemMargin: EdgeInsets.symmetric(horizontal: 5),
+          selectedItemTextPadding: EdgeInsets.symmetric(horizontal: 5),
+          itemTextPadding: EdgeInsets.symmetric(horizontal: 5),
+          itemMargin: EdgeInsets.symmetric(horizontal: 5),
+          //////////////////////////////////////////////////// hover Text 
+          hoverTextStyle: TextStyle(
+            fontSize: Fonts.headlineSmall,
+            fontWeight: FontWeight.w700,
+            color: ColorsApp.Colors_gradientSplachscreen1,
+          ),
+          ///////////////////////////////////////////////// Shodow sid bar
+          decoration: BoxDecoration(
+            color: Colors.white,
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withAlpha((0.1 * 255).toInt()),
+                blurRadius: 12,
+                offset: Offset(2, 4),
+              ),
+            ],
+          ),
+          ///////////////////////////////////////////////// style item in side bar
+          selectedItemDecoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                ColorsApp.Colors_gradientSplachscreen2.withAlpha((0.7 * 255).toInt()),
+                ColorsApp.Colors_gradientSplachscreen1.withAlpha((0.4 * 255).toInt()),
+                ColorsApp.Colors_gradientSplachscreen2.withAlpha((0.7 * 255).toInt()),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+            borderRadius: BorderRadius.circular(12),
+          ),
+          ///////////////////////////////////////////////// selected text style
+          selectedTextStyle: TextStyle(
+            fontSize: Fonts.headlineMedium,
+            fontWeight: FontWeight.w700,
+            color: Colors.white,
+          ),
+          textStyle: TextStyle(
+            fontSize: Fonts.headlineSmall,
+            fontWeight: FontWeight.w700,
+            color: ColorsApp.Colors_gradientSplachscreen1,
+          ),
+        ),
+        ///////////////////////////////////////////////////////////// items
+        items: [
+          SidebarXItem(
+            iconBuilder:
+                (context, selected) => SvgPicture.asset(
+                  ImagesIconApp.Home,
+                  width: 20,
+                //   colorFilter: ColorFilter.mode(
+                //     selected
+                //         ? Colors.black
+                //         : Colors.white,
+                //     BlendMode.srcIn,
+                //   ),
+                ),
+            label: 'Home',
+          ),
+          SidebarXItem(
+            iconBuilder:
+                (context, selected) => SvgPicture.asset(
+                  ImagesIconApp.Home,
+                  width: 20,
+                  // colorFilter: ColorFilter.mode(
+                  //   selected
+                  //       ? Colors.green
+                  //       : ColorsApp.Colors_gradientSplachscreen2,
+                  //   BlendMode.srcIn,
+                  // ),
+                ),
+            label: 'Home',
+          ),
+        ],
+      ),
     );
   }
 }
